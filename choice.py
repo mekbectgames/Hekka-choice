9b213ceffd29bdd78cfc508a5707d56d35a17a85
mekbectgames <114284811+mekbectgames@users.noreply.github.com>
2023-06-16 20:19:19 +0300

    Update choice.py

--- choice.py
+++ choice.py
@@ -2,6 +2,6 @@
 # meta pic: https://img.icons8.com/emoji/344/grapes-emoji.png
 import random
 
-async def choice(self, message: Message):
+async def choice(self, message):
     choice2 = random.choice(f'Успешно {message}!', f'Не успешно {message}!')
     
