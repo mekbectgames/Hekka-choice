24de4dbeb9b6a9efd58e471cc9feeafeeb12440c
mekbectgames <114284811+mekbectgames@users.noreply.github.com>
2023-06-16 21:50:00 +0300

    Update calc.py

--- calc.py
+++ calc.py
@@ -4,7 +4,6 @@ from .. import loader, utils
 @loader.tds
 class MAth(loader.Module):
     def calc(self, numo, do, numt):
-        "<Первое число> <Действие (+, -, :, *, %, **)> <Второе число>"
         if do == ':':
             utils.answer(numo/numt)
         else:
